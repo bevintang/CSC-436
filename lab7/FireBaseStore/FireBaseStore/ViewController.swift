507e45eaa42ab4c3be3fee30d6548d5a6e54f026
Bevin Tang <bevintang@Bevins-MacBook-Air.local>
2018-05-18 14:37:32 -0700

    Made many changes. Added gitTutorial

---
+++ lab7/FireBaseStore/FireBaseStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireBaseStore
+//
+//  Created by Bevin Tang on 5/16/18.
+//  Copyright © 2018 CalPoly Computer Science. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
